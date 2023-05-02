0d4d5a1a6754867f7245f89eb4b564311d04fad1
Masum Billal <mbillal.eee@gmail.com>
2023-05-03 00:15:33 +0600

    apiResource

--- routes/api.php
+++ routes/api.php
@@ -20,8 +20,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
-Route::get('posts', [PostController::class, 'index']);
-Route::post('posts', [PostController::class, 'store']);
-Route::get('posts/{post}', [PostController::class, 'show']);
-Route::put('posts/{post}', [PostController::class, 'update']);
-Route::delete('posts/{post}', [PostController::class, 'destroy']);
+Route::apiResource('posts', PostController::class);
+
+
+// Route::get('posts', [PostController::class, 'index']);
+// Route::post('posts', [PostController::class, 'store']);
+// Route::get('posts/{post}', [PostController::class, 'show']);
+// Route::put('posts/{post}', [PostController::class, 'update']);
+// Route::delete('posts/{post}', [PostController::class, 'destroy']);
